9a4181f574dcd5436778dea6d0acd7bbce04222f
catakeat <catalin.arsulescu@yahoo.com>
2026-05-13 15:47:49 +0300

    plec acas

--- src/main/java/com/mycompany/minapdf/SecondaryController.java
+++ src/main/java/com/mycompany/minapdf/SecondaryController.java
@@ -9,4 +9,8 @@ public class SecondaryController {
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
+     @FXML
+    private void switchToThird() throws IOException {
+        App.setRoot("third");
+    }
 }
--- src/main/java/com/mycompany/minapdf/ThirdController.java
+++ src/main/java/com/mycompany/minapdf/ThirdController.java
@@ -4,8 +4,10 @@
  */
 package com.mycompany.minapdf;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -22,5 +24,12 @@ public class ThirdController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+     @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+      @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
 }
