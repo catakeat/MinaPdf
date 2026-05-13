a9d1e315d281d35e38f23c218d8c77d7f6660411
catakeat <catalin.arsulescu@yahoo.com>
2026-05-13 13:48:30 +0300

    test de comit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.minapdf {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.minapdf to javafx.fxml;
     exports com.mycompany.minapdf;
